e1c3e8cb1567c695e551cb329a50dfb19056e72a
root <2407439@qq.com>
2018-09-13 20:04:08 +0800

    完成设计登录页面

--- docs/init.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE IF NOT EXISTS YunMall default charset utf8 COLLATE utf8_general_ci; 
---
+++ source/YunMall/YunMall.Entity/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YunMall.Entity
+{
+    public class Constants {
+        /// <summary>
+        /// 网站基本信息配置
+        /// </summary>
+        public static string WEB_TITLE { get; set; } = "请设置您的网站标题";
+        public static string WEB_KEYWORDS { get; set; }
+        public static string WEB_DESCRIPTION { get; set; }
+    }
+}
