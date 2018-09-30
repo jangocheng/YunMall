6ba25d0b02b789f6f745d102990bbe72632c7335
root <2407439@qq.com>
2018-09-30 16:56:12 +0800

    完成商铺页面设计布局

--- source/YunMall/YunMall.Web/Content/js/module/shop/index.js
+++ source/YunMall/YunMall.Web/Content/js/module/shop/index.js
@@ -6,7 +6,7 @@
         var data = [
             {
                 pid: 1,
-                productName: "中秋节月饼(广州)",
+                productName: "中秋节月饼中秋节月饼中秋节月饼",
                 amount: 128,
                 count: 1,
                 mainImage: "/Content/upload_image/20180921/636731695907172444.jpg"
@@ -56,33 +56,17 @@
         ];
 
 
+        // 购物车
+        var shopCarData = []; 
+
         //调用分页
         laypage.render({
             elem: 'page'
             , count: data.length
-            , limit: 3
+            , limit: 6
             , jump: function (obj) {
                 jumpPage(obj);
             }
-        });
-
-        $("#goPay").click(function() {
-            layer.prompt({ title: '请输入支付密码', formType: 1 },
-                function(pass, index) {
-                    layer.close(index);
-                    /*layer.prompt({title: '随便写点啥，并确认', formType: 2}, function(text, index){
-                        layer.close(index);
-                        layer.msg('演示完毕！您的口令：'+ pass +'<br>您最后写下了：'+text);
-                    });*/
-                });
-        });
-
-
-        // 选中支付类型
-        $(".payment li").click(function () {
-            $(".payment li").removeClass("payment-selected");
-            $(this).addClass("payment-selected");
-            $("#tradeType").val($(this).data("id"));
         });
 
         function jumpPage(obj) {
@@ -158,26 +142,54 @@
                 var str = "";
                 str += "<tr>";
                 str += "  <td>";
-                str += '      <img src="' + item.mainImage + '" />';
+                str += '      <img style="" src="' + item.mainImage + '" />';
                 str += "</td>";
                 str += "<td>";
                 str += '  <a href="#" class="title">' + item.productName + '</a>';
                 str += "</td>";
                 str += "<td>";
-                str += '  <span class="price"  data-pid="' + item.pid + '" data-amount="' + item.amount + '">' + item.amount + '</span>';
+                str += '  <span class="price" style="font-size: 20px"  data-pid="' + item.pid + '" data-amount="' + item.amount + '">' + item.amount + '</span>';
                 str += "</td>";
                 str += "<td>";
                 str +=
-                    '  <input class="num" type="number" name="name" value="' + item.count + '"/>';
-                str += "</td>";
-                str += "<td>";
-                str += '  <button class="button button-red" name="remove">移除</button>';
+                    '  <button class="button button-blue" name="addShopCar">加入购物车</button>';
                 str += "</td>";
                 str += "</tr>";
                 str += "</tr>";
                 arr.push(str);
             });
-            $("tbody").html(arr.join(''));
-        }
 
+            if (thisData.length <= 0) {
+                $(".table-product")
+                    .html(
+                        '<div style="width: 100%; margin: 0 auto; text-align: center;line-height: 300px">  <img style="width:360px" src="/content/images/empty.png" /> </div>');
+            } else {
+                $("tbody").html(arr.join(''));
+            }
+
+            var list = "";
+
+            // 添加到购物车
+            $("button[name='addShopCar']").click(function () {
+                var name = $(this).parent().prev().prev().text();
+                var pid = $(this).parent().prev().find("span").data("pid");
+                shopCarData.push({
+                    pid: pid,
+                    pname: name,
+                });
+                $("#shopCarCount").text(shopCarData.length);
+                $(this).animate({ width: "90px", height: "35px" }, 300, null, function () {
+                    $(this).animate({ width: "100px", height: "30px" }, 300);
+
+                    if ($("#shopCar").find("li").length < 3) {
+                        var shortName = name;
+                        if (name.length > 15) {
+                            shortName = name.substr(0, 15);
+                        }
+                        list += "<li title=" + name + ">" + shortName + "</li>";
+                        $("#shopCar").html(list);
+                    }
+                });
+            });
+        }
     });
